832a396858ae0ad9e717cd4c070a21f5b9bde75e
Stanislav Melnikov <melnikov.stn@yandex.ru>
2025-03-03 18:15:06 +0300

    kostil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,37 @@
 'use client'
 
+import { useEffect, useState } from "react";
 import "./page.css";
 import { Trigger } from "./trigger";
 
 export default function Home() {
 
-
+      const [date, setDate] = useState<Date>(new Date());
+  
+      const [hour, setHour] = useState<number>(date.getHours());
+      const [min, setMin] = useState<number>(date.getMinutes());
+      const [sec, setSec] = useState<number>(date.getSeconds());
+    
+      console.log(hour)
+      console.log('min', min)
+      console.log('sec', sec)
+    
+      useEffect(() => {
+        setDate(new Date());
+        const intervalID = setInterval(() => {
+          new Date();
+          setDate(new Date());
+          setHour(new Date().getHours());
+          setMin(new Date().getMinutes());
+          setSec(sec => sec+1);
+        }, 1000);
+  
+        return () => clearInterval(intervalID)
+      }, [sec]);
 
   return (
     <div className="wrapper">
-      <Trigger />
+      <Trigger hour={hour} min={min} sec={sec} />
     </div>
   );
 }
--- src/app/trigger.tsx
+++ src/app/trigger.tsx
@@ -1,29 +1,21 @@
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { hours } from "./datas/layot";
 import { Hour } from "./hour";
 
-export const Trigger = () => {
-    const [date, setDate] = useState<Date>(new Date());
+type Tprops = {
+  hour: number;
+  min: number;
+  sec: number;
+}
 
-    const [hour, setHour] = useState<number>(date.getHours());
-    const [min, setMin] = useState<number>(date.getMinutes());
-    const [sec, setSec] = useState<number>(date.getSeconds());
+export const Trigger: FC<Tprops> = ({hour, min, sec}) => {
   
-    console.log(hour)
-    console.log('min', min)
-  
-    useEffect(() => {
-      const intervalID = setInterval(() => {
-        new Date();
-        setDate(new Date());
-        setHour(new Date().getHours());
-        setMin(new Date().getMinutes());
-        setSec(sec => sec+1);
-      }, 1000);
-  
-      return () => clearInterval(intervalID)
-    }, [date, sec, min, hour]);
-  
+  const [flag, setFlag] = useState('');
+
+  useEffect(() => {
+    setFlag(Math.random().toString())
+  }, [sec])
+
     const hstyle = {
       transform: `rotate(${hour*30 + (min * 6) / 12}deg)`
     }
@@ -38,8 +30,8 @@ export const Trigger = () => {
 
     return (<div className="clock_wrapper">
         {hours.map((h, i) => <Hour key={hours[i]} i={i} hours={hours} hour={h} />)}
-        <div className="hourarrow" style={hstyle}></div>
-        <div className="minutearrow" style={mstyle}></div>
+        <div className={`hourarrow ${flag}`} style={hstyle}></div>
+        <div className={`minutearrow ${flag}`} style={mstyle}></div>
         <div className="secundearrow" style={sstyle}></div>
         <div className="point"></div>
       </div>)
