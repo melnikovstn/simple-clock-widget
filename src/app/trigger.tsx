d505b005285d2e381bfcbe598b26320914a63575
Stanislav Melnikov <melnikov.stn@yandex.ru>
2025-03-03 17:40:19 +0300

    test

---
+++ src/app/trigger.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { hours } from "./datas/layot";
+import { Hour } from "./hour";
+
+export const Trigger = () => {
+    const [date, setDate] = useState<Date>(new Date());
+
+    const [hour, setHour] = useState<number>(date.getHours());
+    const [min, setMin] = useState<number>(date.getMinutes());
+    const [sec, setSec] = useState<number>(date.getSeconds());
+  
+    console.log(hour)
+    console.log('min', min)
+  
+    useEffect(() => {
+      const intervalID = setInterval(() => {
+        new Date();
+        setDate(new Date());
+        setHour(new Date().getHours());
+        setMin(new Date().getMinutes());
+        setSec(sec => sec+1);
+      }, 1000);
+  
+      return () => clearInterval(intervalID)
+    }, [date, sec, min, hour]);
+  
+    const hstyle = {
+      transform: `rotate(${hour*30 + (min * 6) / 12}deg)`
+    }
+  
+    const mstyle = {
+      transform: `rotate(${min * 6}deg)`
+    }
+  
+    const sstyle = {
+      transform: `rotate(${sec * 6}deg)`
+    }
+
+    return (<div className="clock_wrapper">
+        {hours.map((h, i) => <Hour key={hours[i]} i={i} hours={hours} hour={h} />)}
+        <div className="hourarrow" style={hstyle}></div>
+        <div className="minutearrow" style={mstyle}></div>
+        <div className="secundearrow" style={sstyle}></div>
+        <div className="point"></div>
+      </div>)
+}
